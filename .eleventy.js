098200bca3d2f9696becae3a5864fa590e9ab0a0
riocinema <digital@riocinema.org.uk>
2026-01-13 22:47:14 +1100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("src/**/.gitkeep");
 
+  eleventyConfig.addFilter("readableDate", (value) => {
+  const d = new Date(value);
+  if (isNaN(d)) return value; // if it's not a date, just return as-is
+  return new Intl.DateTimeFormat("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(d);
+});
+
+
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.addPassthroughCopy({ "admin": "admin" });
 
