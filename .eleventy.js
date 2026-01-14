7cc65d3f321348be6c9b51a0305ab4957587cfc9
riocinema <digital@riocinema.org.uk>
2026-01-14 18:17:19 +1100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,15 @@ module.exports = function (eleventyConfig) {
   }).format(d);
 });
 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("ymd", (dateObj) => {
+    const d = new Date(dateObj);
+    const yyyy = d.getUTCFullYear();
+    const mm = String(d.getUTCMonth() + 1).padStart(2, "0");
+    const dd = String(d.getUTCDate()).padStart(2, "0");
+    return `${yyyy}-${mm}-${dd}`;
+  });
+  
 
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.addPassthroughCopy({ "admin": "admin" });
